d57020364ce85efc3389a540d8395452ccb1d852
ahmad-hmesh <ahmad.hmesh.04@gmail.com>
2026-09-11 17:38:41 +0300

    Convert Nudge to a PWA with manifest, icons, and service worker

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'nudge-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Runs once when the service worker is first installed
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Cleans up old caches when a new version of the service worker activates
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepts every network request the page makes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
